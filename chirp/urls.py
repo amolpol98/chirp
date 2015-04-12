4078518bb2efd3884f05dc9eeb61c18f4fcca1a5
AMOL POL <amolpol98@gmail.com>
2015-04-12 20:07:12 +0530

    Implemented User Login and Sign Up

--- chirp/urls.py
+++ chirp/urls.py
@@ -8,5 +8,9 @@ urlpatterns = patterns('',
     # url(r'^$', 'chirp.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'chirp_app.views.index'),
+    url(r'^login$', 'chirp_app.views.login_view'),
+    url(r'^logout$', 'chirp_app.views.logout_view'),
+    url(r'^signup$', 'chirp_app.views.signup'),
 )
